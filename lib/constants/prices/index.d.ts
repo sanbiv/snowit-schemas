da4a461808e91ef15d4c7a83c03d5193574fd71f
Santi Bivacqua <sanbiv@gmail.com>
2020-04-02 18:40:23 +0200

    lib removed

--- lib/constants/prices/index.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const Price = 9;
--- lib/constants/products/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { ProductType } from 'types/products';
-export { Price } from 'constants/prices';
-export declare const PRODUCT_TYPES: ProductType[];
